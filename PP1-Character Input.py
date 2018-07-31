34f9496b3809ca1e7097dd5068a349a3a8f8110a
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-07-31 10:16:23 -0700

    finished 1st challenge

---
+++ PP1-Character Input.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+if __name__ == '__main__':
+    userName = input('What is your name?')
+    userAge = input('What is your age?')
+    while True:
+        try:
+            userAge = int(userAge)
+            break
+        except ValueError:
+            userAge = input('What is your age?(a whole number please)')
+    thisYear = datetime.now().year
+    repeatTimes = input('How many times should I repeat this message?')
+    while True:
+        try:
+            repeatTimes = int(repeatTimes)
+            break
+        except ValueError:
+            repeatTimes = input('How many times should I repeat this message?(a whole number please)')
+    year100 = int(thisYear) + (100 - userAge)
+    print(f'You will be 100 on the year of {year100}\n' * repeatTimes)
