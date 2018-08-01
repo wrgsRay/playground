64a334680188ef7ee7be37087611e3d5343eead2
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2018-07-31 22:34:48 -0700

    done

--- PP9-Guess Game One.py
+++ PP9-Guess Game One.py
@@ -1,2 +1,35 @@
 import random
 if __name__ == '__main__':
+    answer = random.randint(1, 9)
+    guesses = 0
+    num = input('Guess a number between 1 and 9. Type exit to exit.')
+    print(answer)
+    while True:
+        try:
+            num = int(num)
+            break
+        except ValueError:
+            if num == 'exit':
+                print('goodbye!')
+                break
+            else:
+                num = input('I said: Give me a number')
+    while num != answer:
+        if num > answer:
+            num = input('Your answer was too high. Try again?')
+            guesses += 1
+        else:
+            num = input('Your answer was too low. Try again?')
+            guesses += 1
+        while True:
+            try:
+                num = int(num)
+                break
+            except ValueError:
+                if num == 'exit':
+                    print('goodbye!')
+                    break
+                else:
+                    num = input('I said: Give me a number')
+
+    print(f'Congrats! You got it! The answer was {answer}! It took you {guesses} guesses!')
