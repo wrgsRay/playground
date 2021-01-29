545077f643439587d5928ffbfee2ea953795c097
Raymond Wang <raymondwy@gmail.com>
2021-01-28 23:09:26 -0800

    fun with custom Exception

--- 100_days_of_python/day17_start.py
+++ 100_days_of_python/day17_start.py
@@ -9,9 +9,20 @@ class User:
         user.followers += 1
         self.following += 1
 
+    def block(self, user):
+        if user == self.username:
+            raise InvalidInput(f"Cannot block yourself")
+        print(f"{user} is blocked")
+
 
 user_1 = User('001', 'angela')
 user_2 = User('002', 'jack')
 
 user_1.follow(user_2)
 
+
+class InvalidInput(Exception):
+    pass
+
+
+user_1.block('angela')
