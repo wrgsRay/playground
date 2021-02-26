05a28645de7d4a7cbc4b8fea858e795161c9e97c
Raymond Wang <raymondwy@gmail.com>
2021-02-25 17:05:49 -0800

    Random colored walk

--- 100_days_of_python/day18_start.py
+++ 100_days_of_python/day18_start.py
@@ -1,16 +1,22 @@
 from turtle import Screen, Turtle
-from random import randint, random
+import turtle as t
+from random import choice, randint
 
 tim = Turtle()
+t.colormode(255)
+tim.pensize(10)
+tim.speed(5)
 
 
-sides = 3
-while sides < 11:
-    tim.color(random(), random(), random())
-    for _ in range(sides):
-        tim.forward(100)
-        tim.right(360/sides)
-    sides += 1
+def random_rgb():
+    return randint(0, 255), randint(0, 255), randint(0, 255)
+
+
+for _ in range(100):
+    tim.color(random_rgb())
+    tim.setheading(choice([0, 90, 180, 270]))
+    tim.forward(30)
+
 
 
 
