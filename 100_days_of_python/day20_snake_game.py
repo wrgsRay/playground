461c7f6762e179475ce1d6124f5eb57f50386b3b
Raymond Wang <raymondwy@gmail.com>
2021-03-02 16:26:28 -0800

    better way to create snake

--- 100_days_of_python/day20_snake_game.py
+++ 100_days_of_python/day20_snake_game.py
@@ -7,12 +7,10 @@ screen.title("My Snake Game")
 
 
 snake = list()
-x_pos = 0
 for part in range(3):
     snake.append(Turtle(shape='square'))
     snake[part].color('white')
-    snake[part].goto(x_pos * -1, 0)
-    x_pos += 20
+    snake[part].goto(part * -20, 0)
 
 
 
